5ee9c4938d30416368c37d6eed3fae613f624201
kearney-sp <35611218+kearney-sp@users.noreply.github.com>
2023-07-20 11:23:58 -0700

    Update gcsfuse_run.sh
    Add --keep-alive flag to try to stop gcloud from terminating app

--- gcsfuse_run.sh
+++ gcsfuse_run.sh
@@ -8,12 +8,8 @@ echo "Mounting GCS Fuse."
 gcsfuse --implicit-dirs --debug_gcs --debug_fuse $BUCKET $MNT_DIR 
 echo "Mounting completed."
 
-# Run the web service on container startup. Here we use the gunicorn
-# webserver, with one worker process and 8 threads.
-# For environments with multiple CPU cores, increase the number of workers
-# to be equal to the cores available.
-# Timeout is set to 0 to disable the timeouts of the workers to allow Cloud Run to handle instance scaling.
-exec /env/bin/panel serve /app/hls_gcloud_app.ipynb --address 0.0.0.0 --port 8080 --allow-websocket-origin "*"
+# Run the web service on container startup.
+exec /env/bin/panel serve /app/hls_gcloud_app.ipynb --address 0.0.0.0 --port 8080 --allow-websocket-origin "*" --keep-alive 30000
 
 # Set to -n to exit immediately when one of the background processes terminate.
 wait -y
